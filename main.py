0a58e1c3abbb9709b66ab8a2e046335852cf1d7f
liuyuxiao <liuyuxiao@163.com>
2025-04-21 01:15:42 +0800

    second commit

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ import json
 import pandas as pd
 from torch.autograd import Variable
 import torch
-from models.MFModel import MFModel
+from models.MFModel import MFModel, MFModel_light
 import random
 import torch.nn as nn
 # from utility.parser import parse_args
 # args = parse_args()
 path="data/NETFLIX/raw"
 import time
+
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -46,16 +47,27 @@ def load_interactions(dataset_type, generate_item_ids=False):
     return interaction_data,item_list,user_list
 
 
+
+
 def ensure_consistency_of_items_across_datasets():
     def get_interaction_matrix(item_list, user_list, interations):
-        interaction_matrix = sp.dok_matrix((len(user_list), len(item_list)), dtype=np.float32)
+        # interaction_matrix = sp.dok_matrix((len(user_list), len(item_list)), dtype=np.float32)
+        index_row = []
+        index_col = []
+        value = []
         # print(interations.items())
         for uid, items in interations.items():
             # print(uid,end=" ")
             u = user_list.index(str(uid))
             for item_id in items:
                 i = item_list.index(str(item_id))
-                interaction_matrix[u,i]+=1
+                index_row.append(u)
+                index_col.append(i)
+                value.append(1)
+        
+        indices= torch.tensor([index_row, index_col], dtype=torch.long)  # 行索引和列索引
+        interaction_matrix=torch.sparse_coo_tensor(indices, value, (len(user_list), len(item_list)))
+        
         return interaction_matrix
 
     item_attribute_sheet=pd.read_csv(f"{path}/item_attribute_filter.csv",header=None)
@@ -91,38 +103,61 @@ def update_model(model:nn.Module, loss, learning_rate):
     global time7
     time7=time.time()
 
-    for name_and_param, grad in zip( filter(lambda x:x[1].requires_grad, model.named_parameters()), filter(lambda x:x is not None,grads)):
+    for name_and_param, grad in zip(filter(lambda x:x[1].requires_grad, model.named_parameters()), filter(lambda x:x is not None,grads)):
         name, param = name_and_param
         if grad is None:
             param.data.sub_(0)
         else:
-            print(name, param, grad)
+            # print(name, param, grad)
             param.data.sub_(learning_rate * grad)
     global time8
     time8=time.time()
 def sample_a_batch_of_ids(batch_size:int,  id_pool:list):
-    ids=random.sample(id_pool,batch_size)
+    ids = id_pool.copy()
+    if batch_size<len(id_pool):
+        ids=random.sample(id_pool,batch_size)
     for id in ids:
         id_pool.remove(id)
     return ids
+
+def update_model_light(model:MFModel_light, loss, learning_rate):
+    global time6
+    time6=time.time()
+    grads = torch.autograd.grad(
+        outputs=loss, inputs= model.get_para()
+    )
+    global time7
+    time7=time.time()
+
+    for name_and_param, grad in zip(model.named_parameters(), grads):
+        name, param = name_and_param
+        if grad is None:
+            pass
+        else:
+            # print(name, param, grad)
+            param.data.sub_(learning_rate * grad)
+    model.set_hidden()
+    global time8
+    time8=time.time()
 time3=0
 time4=0
 time5=0
-def calculate_loss(model:nn.Module, input_user_ids:list, input_items_ids:list, mode:str):
+
+
+def calculate_loss(model:nn.Module, input_user_ids:list, input_items_ids:list, mode:str, interaction_matrix):
     global time3
     time3=time.time()
-    out=model(input_user_ids, input_items_ids,mode)
+    out=model(input_user_ids, input_items_ids, mode)
     global time4
     time4=time.time()
 
-    #高耗时
     loss=None
     for u_idx, u in  enumerate(input_user_ids,0):
         for i_idx,i in enumerate(input_items_ids,0):
             if loss is None:
                 loss=(out[u_idx][i_idx] - interaction_matrix[u,i])**2
-                print(type(loss), loss.device)
-                assert False
+                # print(type(loss), loss.device)
+                # assert False
             else:
                 loss+=(out[u_idx][i_idx] - interaction_matrix[u,i])**2
 
@@ -130,47 +165,73 @@ def calculate_loss(model:nn.Module, input_user_ids:list, input_items_ids:list, m
     time5=time.time()
     return loss
 
+def calculate_loss_light(model:MFModel_light, input_user_ids:list, input_items_ids:list, mode:str, interaction_matrix):
+    global time3
+    time3=time.time()
+    out=model(input_user_ids, input_items_ids, mode)
+    global time4
+    
+    #Where time cost comes!!! 55.649s for batch_size256!
+    time4=time.time()
+
+
+    interaction_matrix_shard = interaction_matrix.index_select(dim=0 if mode == "user" else 1, index=torch.tensor(input_user_ids if mode == "user" else input_items_ids)).to_dense().to(device)
+    # print(interaction_matrix_shard.shape, out.shape)
+    # a=interaction_matrix_shard-out
+    # print(a.shape)
+    # interaction_matrix_shard = torch.zeros((len(input_user_ids), len(input_items_ids))).to(device)
+    # for u_idx,u in enumerate(input_user_ids,0):
+    #     for i_idx,i in  enumerate(input_items_ids,0):
+    #         interaction_matrix_shard[u_idx][i_idx]= torch.tensor(interaction_matrix[u,i])
+
+    global time5
+    time5=time.time()
+    loss=None
+    loss = torch.norm(interaction_matrix_shard - out)+  0* torch.norm(model.get_para())
+    return loss
+
+
 if __name__ == "__main__":
     hidden_dim = 8
     total_epoch_number = 100
-    batch_size=16
-    lr=0.0001
+    batch_size=256
+    lr=0.01
 
     interaction_matrix, complete_item_list, complete_user_list = ensure_consistency_of_items_across_datasets()
-    model = MFModel(complete_user_list, complete_item_list, hidden_dim).to(device)
+    model = MFModel_light(complete_user_list, complete_item_list, hidden_dim).to(device)
     N = len(complete_user_list)
     M = len(complete_item_list)
 
+
+
     #training process
-   
     
     for epoch in range(total_epoch_number):
         #reset id pool
         user_id_pool=[u for u in range(N)]
         item_id_pool = [i for i in range(M)]
         time1=time.time()
-        # model.switch_tranining_mode("user")
         while len(user_id_pool)>0:
-            input_user_ids=sample_a_batch_of_ids(batch_size,user_id_pool)
+            input_user_ids=sample_a_batch_of_ids(batch_size,user_id_pool) 
             input_items_ids=item_id_pool
-            print(input_user_ids)
-            time2=time.time()
-            loss=calculate_loss(model,input_user_ids, input_items_ids, "user")
-            time6=time.time()
-            update_model(model, loss, lr)
-            
-            log_info = "Epoch: {}-{} \t{}\{} \tLoss: {:.6f}".format(epoch,"user",N-len(user_id_pool) ,N, loss.item())
-            print(log_info)
-            print("a:{:.3f} b:{:.3f} c:{:.3f} d:{:.3f} e:{:.3f} f:{:.3f} g:{:.3f}".format(time2 -time1, time3-time2, time4-time3, time5-time4, time6-time5, time7-time6,time8-time7))
-            assert False
 
-        # model.switch_tranining_mode("item")
+            time2=time.time()
+            loss=calculate_loss_light(model,input_user_ids, input_items_ids, "user",interaction_matrix)
+            time6=time.time()
+            update_model_light(model, loss, lr/M)
+            
+            log_info = "Epoch: {}-{} \t{}/{} \tLoss: {:.6f}".format(epoch,"user",N-len(user_id_pool) ,N, loss.item())
+            print(log_info)
+            # print("total time:{:.3f} b:{:.3f} c:{:.3f} d:{:.3f} e:{:.3f} f:{:.3f} g:{:.3f}".format(time2 -time1, time3-time2, time4-time3, time5-time4, time6-time5, time7-time6,time8-time7))
+        # print("hello")
+
+        user_id_pool=[u for u in range(N)]
+        item_id_pool = [i for i in range(M)]
         while len(item_id_pool)>0:
             input_user_ids=user_id_pool
             input_items_ids=sample_a_batch_of_ids(batch_size,item_id_pool)
-            loss=calculate_loss(model,input_user_ids, input_items_ids, "item")
+            loss=calculate_loss_light(model,input_user_ids, input_items_ids, "item", interaction_matrix)
+            update_model_light(model, loss, lr/N)
             
-            update_model(model, loss, lr)
-            
-            log_info = "Epoch: {}\t{} \t{}\{} \tLoss: {:.6f}".format(epoch,"item",M-len(item_id_pool) ,N, loss.item())
+            log_info = "Epoch: {}-{} \t{}/{} \tLoss: {:.6f}".format(epoch,"item",M-len(item_id_pool) ,N, loss.item())
             print(log_info)
--- models/MFModel.py
+++ models/MFModel.py
@@ -1,5 +1,7 @@
 import torch.nn as nn
 import torch
+import numpy as np
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class MFModel(nn.Module):
     def __init__(self,user_list,item_list, hidden_dim) -> None:
         super(MFModel, self).__init__()
@@ -49,6 +51,51 @@ class MFModel(nn.Module):
     # def named_parameters(self):
     #     a=super().named_parameters()
     #     return filter(lambda x:x[1].requires_grad ,a)
+
+
+class MFModel_light(nn.Module):
+    def __init__(self,user_list,item_list, hidden_dim) -> None:
+        super(MFModel_light, self).__init__()
+        self.user_list = user_list
+        self.item_list = item_list
+        self.hidden_dim = hidden_dim
+        self.N=len(user_list)
+        self.M=len(item_list)
+        self.user_hiddens = [np.random.random(hidden_dim) for u in range(self.N)]
+        self.item_hiddens = [np.random.random(hidden_dim) for i in range(self.M)]
+
+        # self.user_hiddens = nn.ParameterList([nn.Parameter(torch.rand(hidden_dim),requires_grad=False) for u in range(self.N)])
+        # self.item_hiddens = nn.ParameterList([nn.Parameter(torch.rand(hidden_dim),requires_grad=False) for i in range(self.M)])
+        self.mode="user"
+    def forward(self, user_ids:list, item_ids:list, mode:str="user"):
+        self.curr_user_ids = user_ids
+        self.curr_item_ids = item_ids
+        self.mode = mode
+        if mode=="user":
+            self.user_hidden_ = nn.Parameter(torch.tensor(np.array([self.user_hiddens[u] for u in user_ids]))).to(device)
+            self.item_hidden_ = torch.tensor(np.array([self.item_hiddens[i] for i in item_ids])).to(device)
+        elif mode=="item":
+            self.user_hidden_ = torch.tensor(np.array([self.user_hiddens[u] for u in user_ids])).to(device)
+            self.item_hidden_ = nn.Parameter(torch.tensor(np.array([self.item_hiddens[i] for i in item_ids]))).to(device)
+        else:
+            assert False
+
+        out = torch.mm(self.user_hidden_, self.item_hidden_.T)
+        return out
+    def get_para(self):
+        if self.mode=="user":
+            return self.user_hidden_
+        elif self.mode=="item":
+            return self.item_hidden_
+        else:
+            assert False
+    def set_hidden(self):
+        if self.mode=="user":
+            for u_idx, u in enumerate(self.curr_user_ids):
+                self.user_hiddens[u] =  np.array(self.user_hidden_[u_idx].detach().cpu())
+        elif self.mode=="item":
+            for i_idx, i in enumerate(self.curr_item_ids):
+                self.item_hiddens[i] =  np.array(self.item_hidden_[i_idx].detach().cpu())
 
 
         
